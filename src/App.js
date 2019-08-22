ab66cf5c127f24e6ac62ef8a932451a335b584a5
Naghmeh mohammadi <naghmehmohammadi@Naghmehs-MacBook-Air.local>
2019-08-22 16:57:21 +0800

    git_finder_react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <Navbar />
+      </nav>
+    );
+
+  }
+
+}
+
+export default App;
